053ab4d56272f5a424508a811447c6d713337b00
davidhermosa <davidpereira_10@hotmail.com>
2017-05-03 21:47:29 -0400

    Agregado de la clase Acceso

---
+++ repuestos/repuestos/src/main/java/com/davidhermosa/ipoo/repuestos/datos/cliente/Acceso.java
@@ -0,0 +1,18 @@
+package com.davidhermosa.ipoo.repuestos.datos.cliente;
+
+import java.util.Date;
+
+public class Acceso {
+	
+	private Date fechaHoraInicio;
+	private Date fechaHoraFin;
+	private String ip;
+	private Cliente cliente;
+	
+	
+	public Acceso(){
+		
+	}
+	
+
+}
