1e509e27d70c2d1b54a5a51b7ef8aea6635cb2b0
davidhermosa89 <davidpereira_10@hotmail.com>
2017-06-06 17:42:41 -0400

    Agregar modulos

---
+++ Vehiculo.java
@@ -0,0 +1,44 @@
+package com.grupo5.ipoo.vehiculoAutonomo.vehiculo;
+
+public class Vehiculo {
+	private int potencia;
+
+	public Vehiculo() {
+		super();
+	}
+
+	public int getPotencia() {
+		return potencia;
+	}
+
+	public void setPotencia(int potencia) {
+		this.potencia = potencia;
+	}
+
+	public Vehiculo(int potencia) {
+		super();
+		this.potencia = potencia;
+	}
+	//metodos
+	
+	public void procesarEntorno(){
+		return;
+		
+	}
+	
+	public void procesarOrden(){
+		return;
+		
+	}
+	
+	public void encender(){
+		return;
+	
+	}
+	
+	public void apagar(){
+		return;
+		
+	}
+	
+}
